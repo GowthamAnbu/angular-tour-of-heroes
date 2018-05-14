639539e074cd1db105e1e49b2aa355ae4b506952
Gowtham Anbazhagan <gowthama@tag124.tag.dc>
2018-05-14 16:03:34 +0530

    #6

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,13 +18,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
 
 }
